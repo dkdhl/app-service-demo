03900ccec3882ff0665ac28acf6df74fad08235f
Dickson Kho (DHL IT Services) <dickson.kho@dhl.com>
2023-05-28 22:27:52 +0800

    init

---
+++ src/main/java/com/example/appservicedemo/AppServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.appservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServiceDemoApplication.class, args);
+    }
+
+}
